9986ac4f65b9be2e0be2b150c7e9fa17399f77ae
Constantine Tarasenkov <iamvfx@gmail.com>
2021-05-11 22:37:40 +0300

    Compile on Ubuntu 16.04

---
+++ spdlog-87133ef/build/example/CMakeFiles/example.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/constantine/Desktop/spdlog_binaries_ubuntu_16_04/spdlog-87133ef/example/example.cpp" "example/CMakeFiles/example.dir/example.cpp.o" "gcc" "example/CMakeFiles/example.dir/example.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/constantine/Desktop/spdlog_binaries_ubuntu_16_04/spdlog-87133ef/build/CMakeFiles/spdlog.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
